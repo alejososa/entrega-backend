495bc47744fa4159d5c7db1b48b269e0eae10090
Alejo Sosa <alejososa1987@gmail.com>
2023-09-02 18:58:56 -0300

    arreglando

--- src/managers/products/ProductsMongo.js
+++ src/managers/products/ProductsMongo.js
@@ -1,100 +1,100 @@
-import { productsModels } from '../../db/models/products.model.js'
+import { productsModels } from "../../db/models/products.model.js";
 
 class ProductsMongo {
-    async findAllViews() {
-        try {
-            const products = await productsModels.find()          
-            return products
-        } catch (error) {
-            return error
-        }
+  async findAllViews() {
+    try {
+      const products = await productsModels.find();
+      return products;
+    } catch (error) {
+      return error;
     }
-    ////////// nuevo findAll con paginate////////
+  }
+  ////////// nuevo findAll con paginate////////
 
-    async findAll(obj) {
-        try {
-            const { limit = 3, page = 1, sortProduct_price, ...query } = obj
-            console.log(query);
-            const products = await productsModels.paginate(query, { limit, page, sort: { product_price: sortProduct_price } })
-
-            const info = {
-                status: rest ? "succes" : "error",
-                payload: products.docs,
-                totalPages: products.totalPages,
-                prePage: products.prevPage,
-                nextPage: products.nextPage,
-                page: products.page,
-                hasPrevPage: products.hasPrevPage,
-                hasNextPage: products.hasNextPage,
-                nextPageLink: `http://localhost:8080/api/products?page=${products.nextPage}`,
-                prevPageLink: `http://localhost:8080/api/products?page=${products.prevPage}`,
-
-
-            }
-            return info
-        } catch (error) {
-            return error
-        }
+  async findAll(obj) {
+    try {
+      const { limit=4 , page=1, sortProduct_price= "ASC" } = obj 
+      const response = await productsModels.paginate(
+        {},
+        { limit, page, sort: { product_price: sortProduct_price } }
+      );
+      const info = {
+        status: response.docs.length ? "success" : "error",
+        payload: response.docs,
+        totalPages: response.totalPages,
+        prePage: response.prevPage,
+        nextPage: response.nextPage,
+        page: response.page,
+        hasPrevPage: response.hasPrevPage,
+        hasNextPage: response.hasNextPage,
+        nextPageLink: `http://localhost:8080/api/products?page=${response.nextPage}`,
+        prevPageLink: `http://localhost:8080/api/products?page=${response.prevPage}`,
+      };
+      return info;
+    } catch (error) {
+      return error;
     }
+  }
 
-
-    async createOne(obj) {
-        try {
-            const newProduct = await productsModels.create(obj)
-            return newProduct
-        } catch (error) {
-            return error
-        }
+  async createOne(obj) {
+    try {
+      const newProduct = await productsModels.create(obj);
+      return newProduct;
+    } catch (error) {
+      return error;
     }
+  }
 
-    async findById(id) {
-        try {
-            const product = await productsModels.findById(id)
-            return product
-        } catch (error) {
-            return error
-        }
+  async findById(id) {
+    try {
+      const product = await productsModels.findById(id);
+      return product;
+    } catch (error) {
+      return error;
     }
+  }
 
-    async updateOne(id, obj) {
-        try {
-            const response = await productsModels.findByIdAndUpdate({ _id: id }, { ...obj })
-            return response
-        } catch (error) {
-            return error
-        }
+  async updateOne(id, obj) {
+    try {
+      const response = await productsModels.findByIdAndUpdate(
+        { _id: id },
+        { ...obj }
+      );
+      return response;
+    } catch (error) {
+      return error;
     }
+  }
 
-    async deleteOne(id) {
-        try {
-            const response = await productsModels.findByIdAndDelete(id)
-            return response
-        } catch (error) {
-            return "id not founded"
-        }
+  async deleteOne(id) {
+    try {
+      const response = await productsModels.findByIdAndDelete(id);
+      return response;
+    } catch (error) {
+      return "id not founded";
     }
+  }
 
-    async aggregationMet() {
-        try {
-            const response = await productsModels.aggregate([
-                { $match: { product_code: { $lt: 400 } } },
-                {
-                    $group: {
-                        _id: '$product_description',
-                        product_count: { $count: {} },
-                        promedio_precio: { $avg: '$product_price' },
-                    }
-                },
-                { $sort: { product_count: 1 } }
-                //{$count:"products less than 400"}
-            ])
+  async aggregationMet() {
+    try {
+      const response = await productsModels.aggregate([
+        { $match: { product_code: { $lt: 400 } } },
+        {
+          $group: {
+            _id: "$product_description",
+            product_count: { $count: {} },
+            promedio_precio: { $avg: "$product_price" },
+          },
+        },
+        { $sort: { product_count: 1 } },
+        //{$count:"products less than 400"}
+      ]);
 
-
-            return response
-        } catch (error) {
-            return error
-        }
+      return response;
+    } catch (error) {
+      return error;
     }
+  }
 }
 
-export const productsMongo = new ProductsMongo()
+export const productsMongo = new ProductsMongo();
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -3,40 +3,40 @@ import { Router } from "express";
 import { productsMongo } from "../managers/products/ProductsMongo.js";
 import { productsModels } from "../db/models/products.model.js";
 
-const router = Router()
-
+const router = Router();
 
 //listado de productos renderizados desde "home"
-router.get('/', async (req, res) => {
-    try {
-        const products = await productsMongo.findAllViews();
-    res.render('home', {products} );
-
-    } catch (error) {
-        return error
-    }
+router.get("/", async (req, res) => {
+  try {
+    const products = await productsMongo.findAll({
+      sortProduct_price: "ASC",
+      limit: 5,
+      page: 1,
+    });
+    res.render("home", { products });
+  } catch (error) {
+    return error;
+  }
 });
 
-router.get('/realTimeProducts', async (req, res) => {
-    try {
-        const products = await productsMongo.findAllViews();
-        //const productsJSON = JSON.stringify(products);
-        res.render('realTimeProducts', { products })
-
-    } catch (error) {
-        res.status(500).json({ error: 'Cant obtain products list' });
-    }
+router.get("/realTimeProducts", async (req, res) => {
+  try {
+    const products = await productsMongo.findAllViews();
+    //const productsJSON = JSON.stringify(products);
+    res.render("realTimeProducts", { products });
+  } catch (error) {
+    res.status(500).json({ error: "Cant obtain products list" });
+  }
 });
 
-router.get('/products', async (req,res)=>{
-    try {
-        const products = await productsModels.find()
-        res.render('products', {products});
-    } catch (error) {
-        res.status(500).json({ error: 'Cant obtain products list' });
-    }
-})
-
-
+router.get("/products", async (req, res) => {
+  try {
+    const response = await productsMongo.findAll(req.query);
+    console.log(response)
+    res.render("products", response);
+  } catch (error) {
+    res.status(500).json({ error: "Cant obtain products list" });
+  }
+});
 
 export default router;
