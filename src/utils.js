cb57e5e16bb66df1253ddfe5d243e33f156d1ee9
Alejo Sosa <alejososa1987@gmail.com>
2023-09-19 22:44:08 -0300

    login con passport

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import {dirname} from 'path';
 import { fileURLToPath } from 'url';
 import bcrypt from 'bcrypt';
-
+import jwt from 'jsonwebtoken';
 export const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
@@ -10,6 +10,9 @@ export  const hashData= async (data)=>{
     return bcrypt.hash(data, 10)
 }
 
+export const compareHashedData= async (data, hashedData)=>{
+    return bcrypt.compare(data, hashedData);
+}
 export const compareData= async(data, hashData)=>{
     console.log(data);
     console.log(hashData);
